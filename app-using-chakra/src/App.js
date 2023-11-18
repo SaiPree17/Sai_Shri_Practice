f04b2028ca9ec96513ce86cd336fcee0bb1b8630
SAI-69 <saiprasanth2882002@gmail.com>
2023-11-18 22:15:33 +0530

    Added input forms and search input form eg

--- app-using-chakra/src/App.js
+++ app-using-chakra/src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { InputForm } from './InputForm';
+import { useState } from 'react';
 
 function App() {
+  const [textColorChange, setTextColorChange] = useState('Welcome to our Page')
+  const countryNames = ["Argentina", "Belgium", "Canada", "Djouti", "Ethiopia"]
+  const [searchFilter, setSearchFilter] = useState(countryNames)
+
+  const handleColorChange = (e) => {
+    document.getElementById("myDiv").style.color = "green"
+    setTextColorChange(e.target.value)
+  }
+  if (textColorChange.length === 0) {
+    setTextColorChange("Type anything")
+  }
+
+  const handleSearchFilter = (e) => {
+    const typedValue = e.target.value
+    const filteredValues = countryNames.filter((item) => { return item.toLowerCase().indexOf(typedValue.toLowerCase()) !== -1 });
+    setSearchFilter(filteredValues);
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div id="myDiv" style={{ color: 'red' }}>{textColorChange}</div>
+        <InputForm colorChange={handleColorChange} />
+
+
+        Search Items:<input type="text" style={{ margin: "20px" }} onChange={handleSearchFilter}></input>
+        <div id="countryStyle">  {searchFilter && searchFilter.map((item, index) =>
+          <li key={index}>
+            {item}
+          </li>
+        )}</div>
+
+
       </header>
     </div>
   );
 }
 
 export default App;
+
---
+++ app-using-chakra/src/InputForm.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const InputForm = ({colorChange})=>{
+    return(
+        <input type="text" onChange={colorChange}></input>
+    )
+}
